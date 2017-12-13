587c19bf73dadf944aa1978ee1615efad56552df
Chahat Deep Singh <chahat@umd.edu>
2017-12-12 23:31:22 -0500

    Create install_cudnn.sh

---
+++ install_cudnn.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# install CUDA Toolkit v8.0
+# instructions from https://developer.nvidia.com/cuda-downloads (linux -> x86_64 -> Ubuntu -> 16.04 -> deb (network))
+
+# CUDA_REPO_PKG="cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
+# wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
+# sudo dpkg -i ${CUDA_REPO_PKG}
+# sudo apt-get update
+# sudo apt-get -y install cuda
+
+# install cuDNN v6.0
+CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
+wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE}
+tar -xzvf ${CUDNN_TAR_FILE}
+sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
+sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
+sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
+
+# set environment variables
+export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
