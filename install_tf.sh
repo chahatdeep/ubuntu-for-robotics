274620f90b6803659f19cb8bce0c5ac03aa655d7
Chahat Deep Singh <chahat@umd.edu>
2017-12-12 22:52:55 -0500

    Update install_tf.sh

--- install_tf.sh
+++ install_tf.sh
@@ -7,7 +7,7 @@ sudo pip install tensorflow-gpu
 echo "Installed tf-gpu."
 echo "Updating tf-0.xx to tf-1.4..." && sleep 2
 
-# Search for tf-upgrade list @ https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.4.1-cp36-cp36m-linux_x86_64.whl
+# Search for tf-upgrade list @ https://storage.googleapis.com/tensorflow/
 
 # Updating tf-cpu to 1.4:
 # sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.4.0-cp27-cp34m-linux_x86_64.whl
