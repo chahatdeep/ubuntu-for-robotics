b5e5cf0d9cbd7ba3d4396ffa26a0aab7062b048a
Chahat Deep Singh <chahat@umd.edu>
2017-12-12 20:28:45 -0500

    Create install_ros.sh

---
+++ install_ros.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Installation script to install ROS-Kinetic-base in Ubuntu 16.04
+# Configure your Ubuntu repositories: Configure your Ubuntu repositories to allow "restricted," "universe," and "multiverse."
+
+echo "Are you using Ubuntu 16.04?"
+read -p "If yes, press <ENTER> to continue."
+echo "Setup up the source list"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+echo "Adding packages and key to apt" && sleep 1
+sudo sh -c '. /etc/lsb-release && echo "deb http://mirror.umd.edu/packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list' 
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add - 
+sudo apt-get update 
+
+echo "DO NOT Install any ros-desktop/ros-desktop-full, it will destroy your *OpenCV* by installing PCL."
+echo "Thus, installing ros-kinetic-ros-base."
+echo "Install the specific packages you want later."
+read -p "Press <ENTER> to continue."
+sudo apt install ros-kinetic-ros-base
+
+echo "Initializing rosdep"
+sudo rosdep init
+rosdep update
+
+echo "Sourcing..."
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+echo "Printing ROS Environment:"
+printenv | grep ROS
