12166c5926e132e628f3a8113a3fe7b7699faf3a
Chahat Deep Singh <chahat@umd.edu>
2018-01-31 02:32:04 -0500

    Update install_opencv.sh

--- install_opencv.sh
+++ install_opencv.sh
@@ -15,8 +15,8 @@ sudo apt-get -qq remove ffmpeg x264 libx264-dev
 echo "Installing Dependenices"
 sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg cmake qt5-default checkinstall
 echo "Downloading OpenCV: " $version
-wget -O OpenCV-$version.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-"$version".zip/download
-#wget -O OpenCV-$version.zip https://astuteinternet.dl.sourceforge.net/project/opencvlibrary/opencv-unix/3.4.0/opencv-3.4.0.zip
+#wget -O OpenCV-$version.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-"$version".zip/download
+wget -O OpenCV-$version.zip https://astuteinternet.dl.sourceforge.net/project/opencvlibrary/opencv-unix/3.4.0/opencv-3.4.0.zip
 echo "Installing OpenCV" $version
 unzip OpenCV-$version.zip
 cd opencv-$version
