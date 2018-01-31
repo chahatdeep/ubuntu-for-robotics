d6bd283062ee7a4d15ea98f9db87cfc8968672c4
Chahat Deep Singh <chahat@umd.edu>
2018-01-31 02:30:29 -0500

    Update install_opencv.sh

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 version="$(wget -q -O - http://sourceforge.net/projects/opencvlibrary/files/opencv-unix | egrep -m1 -o '\"[0-9](\.[0-9]+)+' | cut -c2-)"
+version=3.4
 echo "Installing OpenCV: " $version
 echo " in" $PWD
 echo "Tested only on Ubuntu-16.04. It might not work with any other distro."
@@ -15,6 +16,7 @@ echo "Installing Dependenices"
 sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg cmake qt5-default checkinstall
 echo "Downloading OpenCV: " $version
 wget -O OpenCV-$version.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-"$version".zip/download
+#wget -O OpenCV-$version.zip https://astuteinternet.dl.sourceforge.net/project/opencvlibrary/opencv-unix/3.4.0/opencv-3.4.0.zip
 echo "Installing OpenCV" $version
 unzip OpenCV-$version.zip
 cd opencv-$version
