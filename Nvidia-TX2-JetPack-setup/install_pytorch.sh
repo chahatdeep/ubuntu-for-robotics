f2272e132e31f222dcf0a0d042cc3cd5dd9c2496
Chahat Deep Singh <chahat@umd.edu>
2018-03-14 19:03:31 -0400

    Create install_pytorch.sh

---
+++ Nvidia-TX2-JetPack-setup/install_pytorch.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# pyTorch install script for NVIDIA Jetson TX1/TX2,
+# from a fresh flashing of JetPack 2.3.1 / JetPack 3.0 / JetPack 3.1
+#
+# for the full source, see jetson-reinforcement repo:
+#   https://github.com/dusty-nv/jetson-reinforcement/blob/master/CMakePreBuild.sh
+#
+# note:  pyTorch documentation calls for use of Anaconda,
+#        however Anaconda isn't available for aarch64.
+#        Instead, we install directly from source using setup.py
+sudo apt-get install python-pip
+
+# upgrade pip
+pip install -U pip
+pip --version
+# pip 9.0.1 from /home/ubuntu/.local/lib/python2.7/site-packages (python 2.7)
+
+# clone pyTorch repo
+git clone http://github.com/pytorch/pytorch
+cd pytorch
+git submodule update --init
+
+# install prereqs
+sudo pip install -U setuptools
+sudo pip install -r requirements.txt
+
+# Develop Mode:
+python setup.py build_deps
+sudo python setup.py develop
+
+# Install Mode:  (substitute for Develop Mode commands)
+#sudo python setup.py install
+
+# Verify CUDA (from python interactive terminal)
+# import torch
+# print(torch.__version__)
+# print(torch.cuda.is_available())
+# a = torch.cuda.FloatTensor(2)
+# print(a)
+# b = torch.randn(2).cuda()
+# print(b)
+# c = a + b
+# print(c)
