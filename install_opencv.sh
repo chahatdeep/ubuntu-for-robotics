0ec10e116d05e63c67531a838b498336e55c010a
Chahat Deep Singh <chahat@umd.edu>
2018-03-26 18:33:35 -0400

    Update install_opencv.sh

--- install_opencv.sh
+++ install_opencv.sh
@@ -32,4 +32,10 @@ make -j$nThreads
 sudo checkinstall
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
+echo "OpenCV is installed"
+
+echo "Installing Opencv support for Python 3"
+sudo apt install python3-pip
+pip3 install opencv-python
 echo "OpenCV" $version "is ready to use."
+
