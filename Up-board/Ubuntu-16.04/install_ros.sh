2c487c23ef89ce7eea62bf7d05b6db81b8ca720e
Chahat Deep Singh <chahat@umd.edu>
2019-09-25 14:26:41 -0400

    Create install_ros.sh

---
+++ Up-board/Ubuntu-16.04/install_ros.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script for Installing ROS Kinetic Base Only. Can be adapted for other versions as well.
+# Author: Chahat Deep Singh
+# Date: Sep 25, 2019
+########################################
